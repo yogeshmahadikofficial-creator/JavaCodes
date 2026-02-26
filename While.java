063e995efc2260f8302dedf3e0c8de2ebd626d2b
Yogesh Mahadik <yogeshmahadik.official@gmail.com>
2026-02-26 12:23:23 +0530

    while loop

---
+++ While.java
@@ -0,0 +1,9 @@
+public class While {
+    public static void main(String args[]){
+        int counter=0;
+        while(counter<100){
+            System.out.println("Hello World");
+            counter++;
+        }
+    }
+}
