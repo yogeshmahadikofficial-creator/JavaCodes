25b52968c18eca3da129ccd176109184d5b48791
Yogesh Mahadik <yogeshmahadik.official@gmail.com>
2026-02-27 17:09:05 +0530

    SwitchExample

---
+++ SwitchExample.java
@@ -0,0 +1,16 @@
+public class SwitchExample{
+    public static void main(String args[]){
+        int number=1;
+        switch(number){
+            case 1: System.out.println("Tea");
+            break;
+            case 2: System.out.println("Coffee");
+            break;
+            case 3: System.out.println("Pizza");
+            break;
+            default: System.out.println("nothing to want");
+
+        }
+    }
+    
+}
