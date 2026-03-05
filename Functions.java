0f9d0314c5ee7cb5dcb7ea0d8fe46c8e0a3715f7
Yogesh Mahadik <yogeshmahadik.official@gmail.com>
2026-03-05 13:08:58 +0530

    Functions

---
+++ Functions.java
@@ -0,0 +1,13 @@
+public class Functions {
+    public static int multiply(int a, int b) {
+        int product=a*b;
+        return product;
+    }
+    public static void main(String[] args) {
+        int a=3;
+        int b=2;
+        int prod=multiply(a, b);
+        System.out.println("The product is:"+prod);
+    }
+    
+}
